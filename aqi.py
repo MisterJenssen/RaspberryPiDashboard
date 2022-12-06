6a192fdd072bbb1c4175f99db4c6d7b705b6eeb8
Richard Janssen <richardjanssen8@hotmail.com>
2022-12-06 21:25:54 +0100

    Made aqi.py compatible with python 3

--- aqi.py
+++ aqi.py
@@ -2,8 +2,8 @@
 # coding=utf-8
 # "DATASHEET": http://cl.ly/ekot
 # https://gist.github.com/kadamski/92653913a53baf9dd1a8
-#from __future__ import print_function
-import serial, struct, sys, time, json, subprocess
+from __future__ import print_function
+import serial, struct, sys, time, json, subprocess, binascii
 
 DEBUG = 0
 CMD_MODE = 2
@@ -22,8 +22,9 @@ MQTT_HOST = ''
 MQTT_TOPIC = '/weather/particulatematter'
 
 ser = serial.Serial()
-ser.port = "/dev/ttyUSB0"
+ser.port = "/dev/ttyUSB1" # "COM6"
 ser.baudrate = 9600
+
 
 ser.open()
 ser.flushInput()
@@ -31,36 +32,42 @@ ser.flushInput()
 byte, data = 0, ""
 
 def dump(d, prefix=''):
-    print(prefix + ' '.join(x.encode('hex') for x in d))
+    print(prefix + ' '.join(str(x) for x in d))
 
 def construct_command(cmd, data=[]):
     assert len(data) <= 12
     data += [0,]*(12-len(data))
     checksum = (sum(data)+cmd-2)%256
-    ret = "\xaa\xb4" + chr(cmd)
-    ret += ''.join(chr(x) for x in data)
-    ret += "\xff\xff" + chr(checksum) + "\xab"
+    ret = b"\xaa\xb4" + chr(cmd).encode()
+    ret = ret + b''.join(chr(x).encode() for x in data)
+    ret = ret + b"\xff\xff" + chr(checksum).encode() + b"\xab"
 
     if DEBUG:
         dump(ret, '> ')
-    return ret
+    return ret #bytes(ret, 'utf-8')
 
 def process_data(d):
-    r = struct.unpack('<HHxxBB', d[2:])
+    r = struct.unpack(u'<HHxxBB', d[2:])
     pm25 = r[0]/10.0
     pm10 = r[1]/10.0
-    checksum = sum(ord(v) for v in d[2:8])%256
+    checksum = sum( (v) for v in d[2:8])%256
+    print("PM 2.5: {} μg/m^3  PM 10: {} μg/m^3 CRC={}".format(pm25, pm10, "OK" if (checksum==r[2] and r[3]==0xab) else "NOK"))
     return [pm25, pm10]
-    #print("PM 2.5: {} μg/m^3  PM 10: {} μg/m^3 CRC={}".format(pm25, pm10, "OK" if (checksum==r[2] and r[3]==0xab) else "NOK"))
+
+def ov(v):
+    try:
+        return ord(v)
+    except:
+        return ord(str(v))
 
 def process_version(d):
-    r = struct.unpack('<BBBHBB', d[3:])
-    checksum = sum(ord(v) for v in d[2:8])%256
+    r = struct.unpack(u'<BBBHBB', d[3:])
+    checksum = sum((v) for v in d[2:8])%256
     print("Y: {}, M: {}, D: {}, ID: {}, CRC={}".format(r[0], r[1], r[2], hex(r[3]), "OK" if (checksum==r[4] and r[5]==0xab) else "NOK"))
 
 def read_response():
     byte = 0
-    while byte != "\xaa":
+    while byte != b"\xaa":
         byte = ser.read(size=1)
 
     d = ser.read(size=9)
@@ -77,7 +84,7 @@ def cmd_query_data():
     ser.write(construct_command(CMD_QUERY_DATA))
     d = read_response()
     values = []
-    if d[1] == "\xc0":
+    if d[1] == 192: # b"\xc0":
         values = process_data(d)
     return values
 
@@ -107,6 +114,10 @@ def pub_mqtt(jsonrow):
     with subprocess.Popen(cmd, shell=False, bufsize=0, stdin=subprocess.PIPE).stdin as f:
         json.dump(jsonrow, f)
 
+def init():
+    cmd_set_sleep(0)
+    cmd_set_working_period(PERIOD_CONTINUOUS)
+    cmd_set_mode(MODE_QUERY)
 
 if __name__ == "__main__":
     cmd_set_sleep(0)
@@ -115,7 +126,7 @@ if __name__ == "__main__":
     cmd_set_mode(MODE_QUERY);
     while True:
         cmd_set_sleep(0)
-        for t in range(15):
+        for t in range(20):
             values = cmd_query_data();
             if values is not None and len(values) == 2:
               print("PM2.5: ", values[0], ", PM10: ", values[1])
@@ -143,6 +154,6 @@ if __name__ == "__main__":
         if MQTT_HOST != '':
             pub_mqtt(jsonrow)
             
-        print("Going to sleep for 1 min...")
+        print("Going to sleep for 5 min...")
         cmd_set_sleep(1)
-        time.sleep(60)
+        time.sleep(300)
